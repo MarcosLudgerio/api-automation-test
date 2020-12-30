698ff77d5e463c277d8ae8b8f88868f33de5a206
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2020-12-30 13:06:30 -0300

    initialize the project and updating in repository

---
+++ src/main/java/br/edu/ufcg/virtus/courseautomation/CourseAutomationApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufcg.virtus.courseautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufcg/virtus/courseautomation/CourseAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufcg.virtus.courseautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
